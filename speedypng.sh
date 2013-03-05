b40666c1f5db79211697d65c17df6c5061740930
Bran van der Meer <branmovic@gmail.com>
2013-03-05 16:03:07 +0100

    Fixed a bug where a posix system couldnt stat /usr/bin/env python, because of the second parameter

--- speedypng.sh
+++ speedypng.sh
@@ -6,5 +6,5 @@ if sys.version_info < (3, 3):
     print('Python 3.3 or greater is required')
     sys.exit(1)
 
-command = ['/usr/bin/env python', 'lib/speedypng.py'] + sys.argv[1:]
+command = ['/usr/bin/env', 'python', 'lib/speedypng.py'] + sys.argv[1:]
 subprocess.call(command)
