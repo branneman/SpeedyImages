10a1d6f85e624cc9e07ca7aa4e319d526b87a545
Bran van der Meer <branmovic@gmail.com>
2013-03-05 21:34:03 +0100

    batch script now returns the exit code of the python app

--- speedypng.bat
+++ speedypng.bat
@@ -8,13 +8,12 @@ if not exist "%PYTHON%" goto nopython
 
 :: Check if Python is version 3.3 or higher
 python.exe -c "import sys; (exit(0) if sys.version_info >= (3, 3) else exit(1))"
-if %ERRORLEVEL% GEQ 1 goto nopython
+if %errorlevel% GEQ 1 goto nopython
 
 python.exe lib/speedypng.py %*
-goto end
+if %errorlevel% GEQ 1 exit %errorlevel%
+exit
 
 :nopython
 echo Python version 3.3 or higher is required.
 echo  Download from: http://www.python.org/download/
-
-:end
